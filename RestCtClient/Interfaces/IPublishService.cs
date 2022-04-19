fa39ea7660604e55ce64d321602b5ae078d94fff
Vadzim Minutka <Vadzim_Minutka@epam.com>
2022-04-19 18:26:32 +0300

    Added https - for now it works

--- RestCtClient/Interfaces/IPublishService.cs
+++ RestCtClient/Interfaces/IPublishService.cs
@@ -6,15 +6,14 @@ using RestEase;
 namespace RestCtClient.Interfaces
 {
     [SerializationMethods(Query = QuerySerializationMethod.Serialized, Body = BodySerializationMethod.Serialized)]
-    [Header("User-Agent", "RestEase")]
     public interface IPublishService
     {
         [AllowAnyStatusCode]
-        [Get("http://localhost:7159/api/items")]
+        [Get("https://localhost:7159/api/items")]
         Task<IEnumerable<Item>> GetItems([Query] FilteringParameters parameters);
 
         [AllowAnyStatusCode]
-        [Get("http://localhost:7159/api/categories")]
+        [Get("https://localhost:7159/api/categories")]
         Task<IEnumerable<GetCategoryResponse>> GetCategories();
     }
 }
